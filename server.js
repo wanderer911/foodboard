568b2f6cfdda9f37ab063851a8cd1fc5ca61ac18
Andrii Skorokhod <andriis@lan.wixpress.com>
2018-05-04 13:32:05 +0300

    initial

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+const distDir = __dirname + "/public/";
+//app.use(express.static(distDir));
+//app.use(require('./backend/routes'));
+
+app.listen(port,function(){
+	console.log(`app is working on http://localhost:${port}/`);
+});
+
+//if not in router. render index.
+app.get('*', function(req, res) {
+	res.sendFile(distDir+'index.html');
+});
