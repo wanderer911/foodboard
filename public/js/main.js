c42875a51a7082c8fccaa267c9a22260dc66d7fc
Andrii Skorokhod <andriis@lan.wixpress.com>
2018-05-22 17:22:58 +0300

    changes to location

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,16 @@
 import {LettersContainer} from './letters.container.js';
 
-const start = new LettersContainer('dp');
+
+const path = location.pathname.split('/')[1];
+const cities = ['ki','dp'];
+const index = cities.findIndex(el=> el === path);
+let city;
+if (index ===-1){
+    city = 'dp';
+}else{
+    city = cities[index];
+}
+const start = new LettersContainer(city);
 
 start.setMenu().then(()=>{
     start.render();
