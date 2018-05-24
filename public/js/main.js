e6ee314eceaa2a59fb53eac863c80ea7e2e66ede
Andrii Skorokhod <andriis@lan.wixpress.com>
2018-05-24 10:55:04 +0300

    refactor

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,18 @@
 import {LettersContainer} from './letters.container.js';
 
 
-const path = location.pathname.split('/')[1];
-const cities = ['ki','dp'];
-const index = cities.findIndex(el=> el === path);
 let city;
-if (index ===-1){
-    city = 'dp';
-}else{
-    city = cities[index];
-}
+(function (){
+    const path = location.pathname.split('/')[1];
+    const cities = ['ki','dp'];
+    const index = cities.findIndex(el=> el === path);
+    if (index === -1){
+        city = 'dp';
+    }else{
+        city = cities[index];
+    }
+})();
+
 const start = new LettersContainer(city);
 
 start.setMenu().then(()=>{
