8521639259885e9ea7e1d653a5fec61da0211332
wanderer911 <dp160291sas@gmail.com>
2018-08-23 16:47:28 +0300

    weekday added. 8 a.m. reload added

--- public/js/containerRenderer.js
+++ public/js/containerRenderer.js
@@ -2,7 +2,10 @@ import itemsGroupedByAlphabetLettersRenderer from './itemsGroupedByAlphabetLette
 import {DishesComponent} from './dishes.component.js';
 import timerComponent from './timer.js';
 
+
+
 export default function({lettersObject: itemsGrouedpByLetters, listOfDishes}) {
+    const weekdays = ['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday','Friday', 'Saturday'];
     return `
         <div class="header">
                 <div class="header-left-panel">
@@ -11,6 +14,7 @@ export default function({lettersObject: itemsGrouedpByLetters, listOfDishes}) {
                 </div>
             
             <div class="header-empty"></div>
+            <div class="header-right-panel">${weekdays[(new Date()).getDay()]}</div> 
             <div class="header-right-panel" id="time">
                 ${timerComponent()}
             </div>
--- public/js/reloadTomorrow.js
+++ public/js/reloadTomorrow.js
@@ -1,12 +1,35 @@
-export default () => {
-  const getDay = () => (new Date()).getDay() 
-  const today = getDay()
-  const interval = 1000 * 60 * 60;
+// export default () => {
+//   const getDay = () => (new Date()).getDay()
+//   const today = getDay()
+//   const interval = 1000 * 60 * 60;
 
-  setInterval(() => {
-   if (getDay() !== today) {
-     location.reload()
-   }
-  }, interval)
+//   setInterval(() => {
+//     if (getDay() !== today) {
+//       location.reload()
+//     }
+//   }, interval)
 
+// }
+
+
+export default ()=>{
+  const startDate = (() => {
+    const date = new Date();
+    return () => date
+  })();
+  const refreshDate = new Date();
+  if (startDate().getHours() > 7) {
+    refreshDate.setDate(startDate().getDate() + 1);
+    refreshDate.setHours(8);
+    refreshDate.setMinutes(0);
+    refreshDate.setSeconds(0)
+  } else {
+    refreshDate.setHours(8)
+    refreshDate.setMinutes(0);
+    refreshDate.setSeconds(0)
+  }
+  setTimeout(()=>{
+    location.reload();
+  },refreshDate.getTime()-startDate().getTime());
 }
+
