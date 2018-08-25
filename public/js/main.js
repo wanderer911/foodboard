b123846f81d429a250e2d09d4b768524fe452a25
bogdanl <bogdanl@wix.com>
2018-08-25 11:01:52 +0300

    Add every day reloading

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,12 @@
 import containerRenderer from './containerRenderer.js';
 import getCity from './getCity.js';
 import loadTodayDataByCity from './loadTodayDataByCity.js';
+import reloadTomorrow from './reloadTomorrow.js';
 
 const city = getCity();
 
 loadTodayDataByCity(city).then(data => {
     document.body.innerHTML = containerRenderer(data);
 });
+
+reloadTomorrow()
