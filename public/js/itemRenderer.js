dc227c03c23427e79cd1782a36f13c44932452bf
wanderer911 <dp160291sas@gmail.com>
2018-09-05 11:54:38 +0300

    index+1.0

--- public/js/itemRenderer.js
+++ public/js/itemRenderer.js
@@ -3,7 +3,7 @@ export default function ({name,dishId}){
     <div class="person">
         <div class="person-info">
             <div class="person-name">${name}</div>
-            <div class="person-dishnumber"><p>${dishId}</p></div>
+            <div class="person-dishnumber"><p>${dishId+1}</p></div>
         </div>
     </div>
     `;
