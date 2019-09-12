69d7d2d8362ff1777abd36f524ec24242bfa211f
wanderer911 <dp160291sas@gmail.com>
2019-09-12 18:00:22 +0300

    small css fix

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,18 +1,20 @@
-export default function(){
-    setInterval(()=>{
+export default function () {
+    setInterval(() => {
         const string = getFormatedDate();
         const element = document.getElementById('time');
-        element.innerHTML = string;
-        },1000
+        if (element.innerHTML !== string) {
+            element.innerHTML = string;
+        }
+    }, 1000
     )
     return getFormatedDate();
 }
 
 
-function getFormatedDate(){
+function getFormatedDate() {
     const currentDate = new Date();
     const hours = currentDate.getHours();
     const minutes = currentDate.getMinutes();
-    const string = `${hours.toString().length === 2? hours:`0${hours}`}:${minutes.toString().length === 2?minutes:`0${minutes}`}`;
+    const string = `${hours.toString().length === 2 ? hours : `0${hours}`}:${minutes.toString().length === 2 ? minutes : `0${minutes}`}`;
     return string;
 }
